d667894e00e0c9f65e8e14c1d499b47b0cdc565a
akrecev <akrecev@gmail.com>
2025-01-24 23:48:29 +0300

    init

---
+++ src/main/java/com/akretsev/alibou_security/AlibouSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.akretsev.alibou_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlibouSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlibouSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akretsev/alibou_security/AlibouSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akretsev.alibou_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlibouSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
