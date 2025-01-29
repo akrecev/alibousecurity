211b0e5f8167f00eb6acdc1a56759799d01af84c
askretsev <kretsev@yandex.ru>
2025-01-29 13:48:25 +0300

    add UserController

---
+++ src/main/java/com/akretsev/alibousecurity/user/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.akretsev.alibousecurity.user.controller;
+
+import com.akretsev.alibousecurity.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+}
