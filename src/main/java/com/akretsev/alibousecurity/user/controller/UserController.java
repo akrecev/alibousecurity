aead79a7870412757d2ccf690ca62acf1a398e47
akrecev <akrecev@gmail.com>
2025-02-21 12:03:53 +0300

    spotlessApply

--- src/main/java/com/akretsev/alibousecurity/user/controller/UserController.java
+++ src/main/java/com/akretsev/alibousecurity/user/controller/UserController.java
@@ -2,14 +2,13 @@ package com.akretsev.alibousecurity.user.controller;
 
 import com.akretsev.alibousecurity.user.model.ChangePasswordRequest;
 import com.akretsev.alibousecurity.user.service.UserService;
+import java.security.Principal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -18,10 +17,7 @@ public class UserController {
     private final UserService userService;
 
     @PatchMapping
-    public ResponseEntity<?> changePassword(
-            @RequestBody ChangePasswordRequest request,
-            Principal connectedUser
-    ) {
+    public ResponseEntity<?> changePassword(@RequestBody ChangePasswordRequest request, Principal connectedUser) {
         userService.changePassword(request, connectedUser);
         return ResponseEntity.ok().build();
     }
--- src/main/java/com/akretsev/alibousecurity/user/service/UserService.java
+++ src/main/java/com/akretsev/alibousecurity/user/service/UserService.java
@@ -1,13 +1,10 @@
 package com.akretsev.alibousecurity.user.service;
 
 import com.akretsev.alibousecurity.user.model.ChangePasswordRequest;
-import org.springframework.stereotype.Service;
-
 import java.security.Principal;
+import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    public void changePassword(ChangePasswordRequest request, Principal connectedUser) {
-
-    }
+    public void changePassword(ChangePasswordRequest request, Principal connectedUser) {}
 }
